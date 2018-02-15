30bfa22dd8b4e86d4d7b1b84521f388528376524
Jashank Jeremy <jashank@rulingia.com>
2018-02-15 21:43:17 +1100

    switch the light theme!

---
+++ lunarized-light-theme.el
@@ -0,0 +1,7 @@
+(require 'lunarized)
+
+(deftheme lunarized-light "The light variant of the lunarized colour theme")
+
+(create-lunarized-theme 'light 'lunarized-light)
+
+(provide-theme 'lunarized-light)

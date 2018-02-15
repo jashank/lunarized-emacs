11e445cf3b4954f60bdb829f5c3362b222c569dd
Jashank Jeremy <jashank@rulingia.com>
2018-02-15 21:42:28 +1100

    switch the dark mode!

---
+++ lunarized-dark-theme.el
@@ -0,0 +1,7 @@
+(require 'lunarized)
+
+(deftheme lunarized-dark "The dark variant of the lunarized colour theme")
+
+(create-lunarized-theme 'dark 'lunarized-dark)
+
+(provide-theme 'lunarized-dark)
